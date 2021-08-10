cb5f5cd240ffefbbbfa12eed945febf7e98e730b
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2021-08-10 11:39:36 +0900

    signout

--- expenses-ui/src/form/MenuBar.js
+++ expenses-ui/src/form/MenuBar.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles'
+import { Auth } from 'aws-amplify';
+import { SetAuthStateContext } from '../context'
 
 const useStyles = makeStyles((thema) => ({
   toolbar: {
@@ -10,14 +12,24 @@ const useStyles = makeStyles((thema) => ({
   }
 }));
 
+const onClickSignOut = (setAuthState) => {
+  Auth.signOut()
+    .then((response) => {
+      setAuthState(null);
+    })
+    .catch((error)  => {
+      console.log(error)
+    });
+}
 
 export default function MenuBar() {
   const classes = useStyles();
+  const setAuthState = useContext(SetAuthStateContext);
   return (
     <div>
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
-          <Button color="inherit">SIGN OUT</Button>
+          <Button color="inherit" onClick={() =>onClickSignOut(setAuthState)}>SIGN OUT</Button>
         </Toolbar>
       </AppBar>
     </div>
