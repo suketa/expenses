463b4b2e710004716a97e337b5be8425a305cadd
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2021-08-15 22:33:38 +0900

    refactoring by prettier

--- expenses-ui/src/form/New.js
+++ expenses-ui/src/form/New.js
@@ -1,152 +1,156 @@
-import { useState, useContext } from 'react';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import Checkbox from '@material-ui/core/Checkbox';
-import { UserContext } from '../context'
-import { expenses_api } from '../const'
+import { useState, useContext } from "react";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import Checkbox from "@material-ui/core/Checkbox";
+import { UserContext } from "../context";
+import { expenses_api } from "../const";
 
 // TODO: move another file
-const ExpensesButton = ({enabled, onClick, ...props}) => {
+const ExpensesButton = ({ enabled, onClick, ...props }) => {
   return (
-    <Button color="primary" variant="contained" onClick={onClick} disabled={!enabled}>
+    <Button
+      color="primary"
+      variant="contained"
+      onClick={onClick}
+      disabled={!enabled}
+    >
       {props.children}
     </Button>
-  )
-}
+  );
+};
 
 const New = () => {
-  const [ item, setItem ] = useState('');
-  const [ cost, setCost ] = useState(0);
-  const [ income, setIncome ] = useState(false);
-  const [ message, setMessage ] = useState('Expenses')
-  const [ savingEnabled, setSavingEnabled ] = useState(false);
+  const [item, setItem] = useState("");
+  const [cost, setCost] = useState(0);
+  const [income, setIncome] = useState(false);
+  const [message, setMessage] = useState("Expenses");
+  const [savingEnabled, setSavingEnabled] = useState(false);
 
   const user = useContext(UserContext);
 
   const setSaveButtonEnabled = (item, cost, income) => {
-    const enabled = (item.length > 0 || income) && (cost !== 0 && cost !== '')
-    setSavingEnabled(enabled)
-  }
+    const enabled = (item.length > 0 || income) && cost !== 0 && cost !== "";
+    setSavingEnabled(enabled);
+  };
 
   const onChangeItem = (event) => {
     const value = event.target.value;
     setItem(value);
     setSaveButtonEnabled(value, cost, income);
-  }
+  };
 
   const onChangeCost = (event) => {
     const v = event.target.value;
-    const value = (isNaN(v) || !v) ? '' : parseInt(v, 10);
-    setCost(value)
+    const value = isNaN(v) || !v ? "" : parseInt(v, 10);
+    setCost(value);
     setSaveButtonEnabled(item, value, income);
-  }
+  };
 
   const onChangeIncome = () => {
     const value = !income;
     if (value) {
-      setItem('')
+      setItem("");
     }
     setIncome(value);
     setSaveButtonEnabled(item, cost, value);
-  }
+  };
 
   const saveData = () => {
-    setSavingEnabled(false)
-    setMessage('Saving...')
+    setSavingEnabled(false);
+    setMessage("Saving...");
 
     const config = {
-      headers: { Authorization: user.signInUserSession.idToken.jwtToken }
+      headers: { Authorization: user.signInUserSession.idToken.jwtToken },
     };
     const url = expenses_api.post;
     const data = {
       cost: cost,
       key: item,
-      income: income
+      income: income,
     };
 
     // TODO: refactoring, move to another common file
-    axios.post(url, data, config)
-      .then(response => {
+    axios
+      .post(url, data, config)
+      .then((response) => {
         setMessage(response.data.message);
         setSavingEnabled(true);
       })
-      .catch(error => {
-        setMessage(error)
+      .catch((error) => {
+        setMessage(error);
         setSavingEnabled(true);
       });
-  }
+  };
 
   const onKeyPressForm = (event) => {
     // event.preventDefault()
-    if (event.key === 'Enter' && savingEnabled) {
+    if (event.key === "Enter" && savingEnabled) {
       event.preventDefault();
-      saveData()
+      saveData();
     }
-  }
+  };
 
   const onClickSave = (event) => {
     event.preventDefault();
-    saveData()
-  }
+    saveData();
+  };
 
   return (
-    <>
-      <form>
-        <FormGroup onKeyPress={onKeyPressForm}>
-          <div>{message}</div>
-          <FormControl margin="normal">
-            <TextField 
-              required 
-              label="Item" 
-              variant="outlined" 
-              InputLabelProps={{
-                shrink: true,
-              }}
-              disabled={income} 
-              value={item} 
-              onChange={onChangeItem}
-            />
-          </FormControl>
-					<FormControl margin="normal">
-						<TextField 
-							required 
-							label="Cost" 
-							variant="outlined"
-							type="number" 
-							InputLabelProps={{
-								shrink: true,
-							}}
-							name="cost"
-							value={cost} 
-							onChange={onChangeCost}
-						/>
-					</FormControl>
-					<FormControl margin="normal">
-						<FormControlLabel
-							control={
-								<Checkbox
-									checked={income}
-									onChange={onChangeIncome}
-									name="income"
-									color="primary"
-								/>
-							}
-							label="income"
-						/>
-					</FormControl>
-					<FormControl margin="normal">
-						<ExpensesButton onClick={onClickSave} enabled={savingEnabled}>
-							save
-						</ExpensesButton>
-					</FormControl>
-				</FormGroup>
-			</form>
-		</>
-	)
-}
+    <form>
+      <FormGroup onKeyPress={onKeyPressForm}>
+        <div>{message}</div>
+        <FormControl margin="normal">
+          <TextField
+            required
+            label="Item"
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            disabled={income}
+            value={item}
+            onChange={onChangeItem}
+          />
+        </FormControl>
+        <FormControl margin="normal">
+          <TextField
+            required
+            label="Cost"
+            variant="outlined"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name="cost"
+            value={cost}
+            onChange={onChangeCost}
+          />
+        </FormControl>
+        <FormControl margin="normal">
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={income}
+                onChange={onChangeIncome}
+                name="income"
+                color="primary"
+              />
+            }
+            label="income"
+          />
+        </FormControl>
+        <FormControl margin="normal">
+          <ExpensesButton onClick={onClickSave} enabled={savingEnabled}>
+            save
+          </ExpensesButton>
+        </FormControl>
+      </FormGroup>
+    </form>
+  );
+};
 
 export default New;
